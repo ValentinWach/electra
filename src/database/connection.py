8a7ba1a6e51f5c65dbe957fadf81b89545dc5c6d
Samuel Sacher <3.10@web.de>
2024-11-07 20:00:11 +0100

    Definition of more tables

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,7 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, declarative_base
 
@@ -14,14 +12,54 @@ Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class Erststimme(Base):
-    __tablename__ = 'Erststimme'
+    __tablename__ = 'erststimme'
 
     id = Column(Integer, primary_key=True, index=True)
-    wahlkreis = relationship('Wahlkreis', back_populates='')
+
+    #Wahlkreis
+    wahlkreis = relationship('Wahlkreis', back_populates='erststimmen')
+    wahlkreis_id = Column(Integer, ForeignKey('wahlkreis.id'), nullable=False)
+
+    #Partei
+    partei = relationship('Partei', back_populates='erststimmen')
+    partei_id = Column(Integer, ForeignKey('partei.id'), nullable=False)
+
+class Zweitstimme(Base):
+    __tablename__ = 'zweitstimme'
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    #Wahlkreis
+    wahlkreis = relationship('Wahlkreis', back_populates='zweitstimmen')
+    wahlkreis_id = Column(Integer, ForeignKey('wahlkreis.id'), nullable=False)
+
+    #Partei
+    partei = relationship('Partei', back_populates='zweitstimmen')
+    partei_id = Column(Integer, ForeignKey('partei.id'), nullable=False)
+
+class Partei(Base):
+    __tablename__ = 'partei'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+
+    #Erstimmen
+    erststimmen = relationship('Erststimme', back_populates="partei")
+
+    #Zweitstimmen
+    zweitstimmen = relationship('Zweitstimme', back_populates='partei')
+
+
 
 class Wahlkreis(Base):
-    __tablename__ = 'Wahlkreis'
+    __tablename__ = 'wahlkreis'
 
     id = Column(Integer, primary_key=True, index=True)
+
+    #Erststimmen
+    erststimmen = relationship('Erststimme', back_populates='wahlkreis')
+
+    #Zweitstimmen
+    zweitstimmen = relationship('Zweitstimme', back_populates='wahlkreis')
 
 
