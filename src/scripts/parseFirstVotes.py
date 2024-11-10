c8050801c1210520c2a4ff8e276409c7b6a3a56b
Samuel Sacher <3.10@web.de>
2024-11-10 17:55:35 +0100

    Improved parseFirstVotes

--- src/scripts/parseFirstVotes.py
+++ src/scripts/parseFirstVotes.py
@@ -17,59 +17,60 @@ engine = create_engine(DATABASE_URL)
 
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
-
 df = pd.read_csv(Path('sourcefiles', 'kerg2.csv'), delimiter=';')
-
 
 # Filtern der Zeilen, bei denen 'Stimme' == 1
 filtered_df = df[(df['Stimme'] == 1) & (df['Gruppenart'] == 'Partei') & (df['Gebietsart'] == 'Wahlkreis')]
-
 
 # Session starten
 db = Session()
 
 # Für jedes gefilterte Tupel ein neues Objekt erstellen und in die Datenbank einfügen
-batch_size = 10000
+batch_size = 100000
 batch = []
 
 counter = 0
 for index, row in filtered_df.iterrows():
     print(f"Count: {counter}")
     counter += 1
-    count = int(row['Anzahl']) if not pd.isna(row['Anzahl']) else 0
+
+    if pd.isna(row['Anzahl']):
+        continue
+
+    count = int(row['Anzahl'])
 
     date_str = row['Wahltag']  # Assuming this is in 'DD.MM.YYYY' format, like '26.09.2021'
     wahl_date = datetime.strptime(date_str, '%d.%m.%Y').date()
 
     wahl = db.query(Wahl).filter_by(
         date=wahl_date,
-    ).one_or_none()
+    ).one()
 
     wahlkreis = db.query(Wahlkreis).filter_by(
         name=row['Gebietsname'],
-    ).one_or_none()
+    ).one()
 
     partei = db.query(Partei).filter_by(
         shortName=row['Gruppenname'],
-    ).one_or_none()
+    ).one()
 
     wahlkreiskandidatur = db.query(Wahlkreiskandidatur).filter_by(
         wahlkreis_id=wahlkreis.id,
         wahl_id=wahl.id,
         partei_id=partei.id
-    ).one_or_none()
+    ).one()
+
+    erststimme = Erststimme(
+        wahlkreis_id=wahlkreis.id,
+        wahlkreiskandidatur_id=wahlkreiskandidatur.id,
+        wahl_id=wahl.id,
+    )
 
     # Inner loop to insert 'Erststimme' objects 'count' times
     for _ in range(count):
         # Create a new Erststimme object with the necessary data
-        erststimme = Erststimme(
-            wahlkreis_id=wahlkreis.id,
-            wahlkreiskandidatur_id=wahlkreiskandidatur.id,
-            wahl_id=wahl.id,
-        )
-        batch.append(erststimme)
 
+        batch.append(erststimme)
         if len(batch) >= batch_size:
             db.bulk_save_objects(batch)
             db.commit()
