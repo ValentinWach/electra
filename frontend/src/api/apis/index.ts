9cc2a03f389aae3c1978318faae005fdc5d0eb1b
Samuel Sacher <3.10@web.de>
2024-11-29 17:35:50 +0100

    Deleted example code, Initialized tailwindCSS, ran API generation for the first time

---
+++ frontend/src/api/apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './GeneralApi';
+export * from './GlobalApi';
+export * from './ResultsApi';
+export * from './SpecialApi';
+export * from './WahlkreisApi';
---
+++ frontend/src/api/models/index.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Abgeordneter';
+export * from './ClosestWinners';
+export * from './OverviewWahlkreis';
+export * from './Partei';
+export * from './SeatDistribution';
+export * from './SeatDistributionDistributionInner';
+export * from './Stimmanteil';
+export * from './Wahl';
+export * from './WinningParties';
+export * from './WinningPartiesErststimmeInner';
+export * from './WinningPartiesZweitstimmeInner';
