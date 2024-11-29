abe5ac93c4f596b7709c1d19a961194d07a01e6e
Samuel Sacher <3.10@web.de>
2024-11-29 20:14:19 +0100

    Added first version of sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import {fetchWahlen, fetchSitzveteilung} from './apiServices';
 import {SeatDistribution, Wahl} from "./api";
+import Sidebar from './Sidebar';
 
 function App() {
     const [wahlen, setWahlen] = useState<Wahl[]>([]);
@@ -31,25 +32,51 @@ function App() {
     });
 
     return (
-        <>
-            <div className={"bg-red-400"}>
-                <h1 className={"text-blue-400"}>Wahlen</h1>
-                <ul>
-                    {wahlen.map((wahl) => (
-                        <li key={wahl.id}>{wahl.name} - {wahl.date.toString()}</li>
-                    ))}
-                </ul>
+        <div className={"flex h-screen"}>
+            <div className={"w-64"}>
+                <Sidebar/>
             </div>
-            <div>
-                <h1 className={"text-blue-400"}>Sitzverteilung</h1>
-                <p>Anzahl Sitze: {sitzverteilung?.numberofseats}</p>
-                <ul>
-                    {sitzverteilung?.distribution.map((partei) => (
-                        <li key={partei.party.id}>{partei.party.shortname} : {partei.seats}</li>
-                    ))}
-                </ul>
+            <div className={""}>
+                <div className={"pt-40"}>
+                    <h1>Wahlen</h1>
+                    <ul>
+                        {wahlen.map((wahl) => (
+                            <li key={wahl.id}>{wahl.name} - {wahl.date.toString()}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <h1>Sitzverteilung</h1>
+                    <p>Anzahl Sitze: {sitzverteilung?.numberofseats}</p>
+                    <table className="min-w-full divide-y divide-gray-200">
+                        <thead className="bg-gray-50">
+                        <tr>
+                            <th scope="col"
+                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Partei
+                            </th>
+                            <th scope="col"
+                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Sitze
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                        {sitzverteilung?.distribution.map((partei) => (
+                            <tr key={partei.party.id}>
+                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    {partei.party.shortname}
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    {partei.seats}
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
