30d165ac38b34521873005beb9d4c4749662352c
Samuel Sacher <3.10@web.de>
2025-01-27 16:41:06 +0100

    refreshing mat views in correct order

--- backend/database-tools/sitzverteilung/refresh-materialized-views.sql
+++ backend/database-tools/sitzverteilung/refresh-materialized-views.sql
@@ -1,6 +1,7 @@
 BEGIN;
 ----------Basisdaten----------
 
+-- Base data views must be refreshed first
 REFRESH MATERIALIZED VIEW zweitstimmen_wahlkreis_partei;
 REFRESH MATERIALIZED VIEW zweitstimmen_bundesland_partei;
 REFRESH MATERIALIZED VIEW zweitstimmen_partei;
@@ -8,16 +9,16 @@ REFRESH MATERIALIZED VIEW wahlkreis_winners;
 
 ----------Berechnungen----------
 REFRESH MATERIALIZED VIEW bundestag_parties;
-REFRESH MATERIALIZED VIEW wahlkreissitze_parteien_bundesweit;
-REFRESH MATERIALIZED VIEW mindestsitzanspruch_partei_bundesland;
-REFRESH MATERIALIZED VIEW ov_2_sitzkontingente_bundesweit_erhoeht_basis;
-REFRESH MATERIALIZED VIEW abgeordnete;
 
 --Depends on dynamic statement and can thus not be a materialized view
 TRUNCATE TABLE uv_1_sitzkontingente_landeslisten;
 INSERT INTO uv_1_sitzkontingente_landeslisten (wahl_id, bundesland_id, partei_id, sitze)
 SELECT wahl_id, bundesland_id, partei_id, sitze
 FROM calculate_seats_per_party_per_bundesland_and_election();
+
+REFRESH MATERIALIZED VIEW wahlkreissitze_parteien_bundesweit;
+REFRESH MATERIALIZED VIEW mindestsitzanspruch_partei_bundesland;
+REFRESH MATERIALIZED VIEW ov_2_sitzkontingente_bundesweit_erhoeht_basis;
 
 --Depends on dynamic statement and can thus not be a materialized view
 TRUNCATE TABLE ov_2_sitzkontingente_bundesweit_erhoeht;
@@ -28,4 +29,7 @@ SELECT wahl_id, partei_id, stimmen_sum, mindestsitzanspruch, verbleibender_ueber
 TRUNCATE TABLE uv_2_sitzkontingente_landeslisten_erhoeht;
 INSERT INTO uv_2_sitzkontingente_landeslisten_erhoeht (wahl_id, bundesland_id, partei_id, landeslistensitze, stimmen_sum, mindestsitzanspruch, sum)
 SELECT calc.wahl_id, calc.bundesland_id, calc.partei_id, calc.landeslistensitze, calc.stimmen_sum, calc.mindestsitzanspruch, calc.sitze_final as sum FROM calculate_increased_seats_per_party_per_bundesland_and_election() calc;
+
+REFRESH MATERIALIZED VIEW abgeordnete;
+
 COMMIT;
